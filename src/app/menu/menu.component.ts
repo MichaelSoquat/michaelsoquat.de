db0e18ac1bdf07f6d638693e9b8c3ed00102ea60
MichaelSoquat <michael.soquat@gmx.de>
2022-02-23 10:04:37 +0100

    testing menu links

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,9 +8,33 @@ import { Router } from '@angular/router';
 })
 export class MenuComponent implements OnInit {
   @Input() darkMode = true;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 
+  toMySkill() {
+    document.getElementById('contactMe')?.scrollIntoView({ block: 'center' });
+  }
+
+  toMyWork() {
+    document.getElementById('contactMe')?.scrollIntoView({ block: 'center' });
+  }
+
+  toAboutMe() {
+    document.getElementById('contactMe')?.scrollIntoView({ block: 'center' });
+  }
+
+  toMyContact() {
+    document.getElementById('contactMe')?.scrollIntoView({ block: 'center' });
+  }
 }
