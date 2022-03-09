9abf6f48b1ada7c312062fb4d30d6ee5e2250234
MichaelSoquat <michael.soquat@gmx.de>
2022-03-09 21:36:01 +0100

    added animation to my skills

--- src/app/main/my-skills/my-skills.component.ts
+++ src/app/main/my-skills/my-skills.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as AOS from "aos"
 @Component({
   selector: 'app-my-skills',
   templateUrl: './my-skills.component.html',
@@ -10,6 +10,7 @@ export class MySkillsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init();
   }
 
 }
